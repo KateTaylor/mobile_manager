16c6ec92d8c95bec70b20ee58404e64a4c8b98ed
Kate Taylor <kt6@nc-sa4500-017.internal.sanger.ac.uk>
2011-11-30 20:25:20 +0000

    Add simple jasmine Javascript checker, tidy up test directories.

---
+++ test/jasmine/spec/html/ApplicationSpec.js
@@ -0,0 +1,11 @@
+describe("Application", function() {
+	describe('url_root',function(){
+  	it('is correct url',function(){
+    	expect(url_root).toBe('http://psd-dev.internal.sanger.ac.uk');
+// expect(next_url).toStartWith(url_root)
+
+// expect every function named in init() to be defined somewhere
+
+  	});
+  });
+});
